bad2590ed293880a0462f5498a7eaa5423d8baf9
Alan Lara <AlanDasein@users.noreply.github.com>
2014-12-16 20:55:22 -0800

    Update Tools.php

--- Tools.php
+++ Tools.php
@@ -1,39 +1,39 @@
 <?php
-
-class Tools
-{
 	
-	public function __construct() {}
-	
-	public function get_mac($_ip) {
+	class Tools
+	{
 		
-		$mac = 'WAN Connection';
-		$arp = `arp -a $_ip`;
-		$lines = explode("\n", $arp);
+		public function __construct() {}
 		
-		foreach($lines as $line) {
-			$cols = preg_split('/\s+/', trim($line));
-			if($cols[0] == $ip) {$mac = $cols[1];}
+		public function get_mac($_ip) {
+			
+			$mac = 'WAN Connection';
+			$arp = `arp -a $_ip`;
+			$lines = explode("\n", $arp);
+			
+			foreach($lines as $line) {
+				$cols = preg_split('/\s+/', trim($line));
+				if($cols[0] == $ip) {$mac = $cols[1];}
+			}
+			
+			return $mac;
+			
 		}
 		
-		return $mac;
+		public function get_url($_url){
+			
+			$ch = curl_init();
+			
+			curl_setopt($ch, CURLOPT_URL, $_url);
+			curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+			
+			$response = curl_exec($ch);
+			
+			curl_close($ch);
+			
+			return $response;
+			
+		}
 		
 	}
-	
-	public function get_url($_url){
-		
-		$ch = curl_init();
-		
-		curl_setopt($ch, CURLOPT_URL, $_url);
-		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		
-		$response = curl_exec($ch);
-		
-		curl_close($ch);
-		
-		return $response;
-		
-	}
-	
-}
