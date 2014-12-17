a877ab3782826f03a7d298baede3f89b775ea209
Alan Lara <AlanDasein@users.noreply.github.com>
2014-12-16 20:54:16 -0800

    Update Files.php

--- Files.php
+++ Files.php
@@ -1,38 +1,38 @@
 <?php
 
-class Files
-{
-	
-	public function __construct() {}
-	
-	public function remote_file_exists($_url) {
-		$fp = @fopen($_url, 'r');
-		if($fp !== false){fclose($_url);}
-		return($fp);
-	}
-	
-	public function delete_dir($_dir) {
+	class Files
+	{
 		
-		if(is_dir($_dir)) {
+		public function __construct() {}
+		
+		public function remote_file_exists($_url) {
+			$fp = @fopen($_url, 'r');
+			if($fp !== false){fclose($_url);}
+			return($fp);
+		}
+		
+		public function delete_dir($_dir) {
 			
-			$objects = scandir($_dir);
-			
-			foreach($objects as $object) {
+			if(is_dir($_dir)) {
 				
-				if($object != "." && $object != "..") {
+				$objects = scandir($_dir);
+				
+				foreach($objects as $object) {
 					
-					if(filetype($_dir."/".$object) == "dir") {rrmdir($_dir."/".$object);}
-					else {unlink($_dir."/".$object);}
+					if($object != "." && $object != "..") {
+						
+						if(filetype($_dir."/".$object) == "dir") {rrmdir($_dir."/".$object);}
+						else {unlink($_dir."/".$object);}
+						
+					}
 					
 				}
 				
+				reset($objects);
+				rmdir($_dir);
+				
 			}
-			
-			reset($objects);
-			rmdir($_dir);
 			
 		}
 		
 	}
-	
-}
